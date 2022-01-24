b3db182168f381c94850728152da91415b85a719
Vinod Mattoo <rahulmattoo0@gmail.com>
2022-01-24 10:47:36 +0530

    add and clear functionality
    Signed-off-by: Vinod Mattoo <rahulmattoo0@gmail.com>

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ let btnAdd = $('#btnAdd');
 let btnClear = $('#btnClear');
 let inpNewTask = $('#inpNewTask');
 
-btnAdd.click(() => {
+btnAdd.click(addItem);
+
+btnClear.click(() => inpNewTask.val(''));
+
+inpNewTask.keypress((e) => {
+    if (e.which == 13) {
+        addItem();
+    }
+})
+
+function addItem() {
     console.log(inpNewTask.val());
+
+    var listItem = $('<li>', {
+        class: 'list-group-item',
+        text: inpNewTask.val()
+    });
+
+    ulTasks.append(listItem);
+
     inpNewTask.val('');
-});
+}
 
