4e9baeb4387d680cf6d1d985f6844cd675c22f6b
Vinod Mattoo <rahulmattoo0@gmail.com>
2022-01-26 18:40:58 +0530

    cleanup and sort button functionality
    Signed-off-by: Vinod Mattoo <rahulmattoo0@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let ulTasks = $('#ulTasks');
 let btnAdd = $('#btnAdd');
 let btnClear = $('#btnClear');
+let btnCleanup = $('#btnCleanup');
 let inpNewTask = $('#inpNewTask');
+let btnSort = $('#btnSort');
 
 btnAdd.click(addItem);
 
@@ -16,13 +18,29 @@ inpNewTask.keypress((e) => {
 function addItem() {
     console.log(inpNewTask.val());
 
+    if (inpNewTask.val() == '') return;
+
     var listItem = $('<li>', {
         class: 'list-group-item',
         text: inpNewTask.val()
     });
+
+    listItem.click(() => {
+        listItem.toggleClass('done');
+    })
 
     ulTasks.append(listItem);
 
     inpNewTask.val('');
 }
 
+function clearDone() {
+    $('#ulTasks .done').remove();
+}
+
+function sortItems() {
+    $('#ulTasks .done').appendTo(ulTasks);
+}
+
+btnCleanup.click(clearDone);
+btnSort.click(sortItems);
